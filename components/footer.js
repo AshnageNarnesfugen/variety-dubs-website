8d9cc57ed83adfd21ddfa10675e07bfffe50a3c0
Ashnage Narnesfugen <50156611+AshnageNarnesfugen@users.noreply.github.com>
2022-10-03 23:51:28 -0500

    yeah

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <>
-            <footer className="bg-slate-200 pt-5 pb-5">
+            <footer className="bg-slate-200 pt-5 pb-5 px-10">
                 <div className="container mx-auto">
                     <div className="row flex">
                         <div className="w-6/12">
